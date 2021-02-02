95937d36372384fb4f4995b56697c04bde098665
Daniel Litt <daniel_litt@comcast.net>
2021-02-02 13:10:54 -0500

    Add init function for Three.js elements

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-const scene = new THREE.Scene();
+let scene, camera, renderer, cube;
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+function init() {
+    scene = new THREE.Scene();
 
-const renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setClearColor("#949db0");
-renderer.setSize(window.innerWidth, window.innerHeight);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
-document.body.appendChild(renderer.domElement);
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setClearColor("#949db0");
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+    const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+    cube = new THREE.Mesh( geometry, material );
+    scene.add(cube);
+
+    camera.position.z = 5;
+}
 
 window.addEventListener("resize", () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -14,17 +25,13 @@ window.addEventListener("resize", () => {
     camera.updateProjectionMatrix();
 });
 
-const geometry = new THREE.BoxGeometry( 2, 2, 2 );
-const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-const cube = new THREE.Mesh( geometry, material );
-scene.add(cube);
-
-camera.position.z = 5;
-
 function animate() {
     requestAnimationFrame(animate);
 
     renderer.render(scene, camera);
+
     cube.rotation.y += 0.01;
 }
+
+init();
 animate();
