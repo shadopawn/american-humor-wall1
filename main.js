8bc007370ac12c09d3b256cafeccee4fa6727fa9
Daniel Litt <daniel_litt@comcast.net>
2021-02-02 11:59:44 -0500

    Create scene and add cube

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("Hello");
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setClearColor("#949db0");
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+window.addEventListener("resize", () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth/window.innerHeight;
+    camera.updateProjectionMatrix();
+});
+
+const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+const cube = new THREE.Mesh( geometry, material );
+scene.add(cube);
+
+camera.position.z = 5;
+
+renderer.render(scene, camera)
