4faa18bde54ffe7479d5916a556b67bf0b5be482
Daniel Litt <daniel_litt@comcast.net>
2021-03-08 15:40:20 -0500

    Try another alternative get raw for audio player

--- scripts/playQuoteAudio.js
+++ scripts/playQuoteAudio.js
@@ -1,6 +1,6 @@
 let audio = new Audio();
 function playQuoteAudio(fileName) {
-    let url = "https://shadopawn.github.io/assets/audio/"+fileName +"?raw=true"
+    let url = "https://github.com/shadopawn/3d-web-prototype/blob/master/assets/audio/" + fileName + "?raw=true"
     audio.pause();
     audio.src = url;
     audio.play();
