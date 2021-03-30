f29d733f6298b9f30fe3bb0b67fa2279b043ae12
Daniel Litt <daniel_litt@comcast.net>
2021-03-30 14:47:07 -0400

    Update time line variable names

--- scripts/orbitText.js
+++ scripts/orbitText.js
@@ -20,20 +20,20 @@ const orbitTextController = new ScrollMagic.Controller();
 
 function addOrbitAnimations(element, start){
     //Add animation to rotate in
-    let rotateTimeLine = new TimelineMax();
-    rotateTimeLine.to(element, 4, {x: 0, rotationY: 0, opacity: 3});
+    let rotateInTimeLine = new TimelineMax();
+    rotateInTimeLine.to(element, 4, {x: 0, rotationY: 0, opacity: 3});
 
     new ScrollMagic.Scene({
         duration: rotateInOutDuration, 
         offset: start
     })
-        .setTween(rotateTimeLine)
+        .setTween(rotateInTimeLine)
         .addTo(orbitTextController);
 
 
     //Add animation to rotate out
-    let fadeTimeLine = new TimelineMax();
-    fadeTimeLine.to(element, 4, {x: 400, rotationY: 70, opacity: 0});
+    let rotateOutTimeLine = new TimelineMax();
+    rotateOutTimeLine.to(element, 4, {x: 400, rotationY: 70, opacity: 0});
 
     const rotateOut = start + staticDistance + rotateInOutDuration;
 
@@ -41,6 +41,6 @@ function addOrbitAnimations(element, start){
         duration: rotateInOutDuration,
         offset: rotateOut
     })
-        .setTween(fadeTimeLine)
+        .setTween(rotateOutTimeLine)
         .addTo(orbitTextController);
 }
