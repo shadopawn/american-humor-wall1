19933819b40a02c7d145405d458536443884df81
Daniel Litt <daniel_litt@comcast.net>
2021-04-17 14:57:17 -0400

    Apply style changes to getScrollSpeed

--- scripts/main.js
+++ scripts/main.js
@@ -252,30 +252,28 @@ function enableModelRotation(){
     modelRotationEnabled = true;
 }
 
-var getScrollSpeed = (function(settings){
-    settings = settings || {};
-
-    var lastPos, newPos, timer, delta, 
-        delay = settings.delay || 50; // in "ms" (higher means lower fidelity)
+let getScrollSpeed = function(){
+    let lastPos, newPos, timer, delta;
+    delay = 50; // in "ms" (higher means lower fidelity)
 
     function clear() {
-      lastPos = null;
-      delta = 0;
+        lastPos = null;
+        delta = 0;
     }
 
     clear();
 
-    return function(){
-      newPos = window.scrollY;
-      if ( lastPos != null ){ // && newPos < maxScroll 
-        delta = newPos -  lastPos;
-      }
-      lastPos = newPos;
-      clearTimeout(timer);
-      timer = setTimeout(clear, delay);
-      return delta;
-    };
-})();
+    return () =>{
+        newPos = window.scrollY;
+        if (lastPos != null){ 
+            delta = newPos - lastPos;
+        }
+        lastPos = newPos;
+        clearTimeout(timer);
+        timer = setTimeout(clear, delay);
+        return delta;
+    }
+}();
 
 let mouseMoveEvent;
 window.addEventListener("mousemove", onMouseMove);
